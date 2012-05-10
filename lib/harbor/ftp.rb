55f3310570a216cf46030f84a40ee189b93e83e8
Sam Smoot <ssmoot@gmail.com>
2012-05-10 11:18:55 -0500

    Removed FTP::user_database method since in an embedded scenario you're likely to have already setup your Sequel connection. Instead made requiring the Sequel based user_manager optional.

--- lib/harbor/ftp.rb
+++ lib/harbor/ftp.rb
@@ -6,13 +6,3 @@ require "java"
 Dir[Pathname(__FILE__).dirname.parent.parent + "jars" + "*.jar"].each { |jar| require jar }
 
 require "harbor/ftp/server"
-
-class Harbor
-  module FTP
-    def self.user_database(connection_string)
-      require "sequel"
-      Sequel.connect(connection_string)
-      require "harbor/ftp/user_manager"
-    end
-  end
-end
