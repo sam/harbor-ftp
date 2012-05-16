fa5e46672cda0207125fa04a2ad7fd2c0a830a2a
Sam Smoot <ssmoot@gmail.com>
2012-05-16 15:33:55 -0500

    Just added a few more comments to examples/standalone-server.rb.

--- examples/standalone-server.rb
+++ examples/standalone-server.rb
@@ -17,12 +17,18 @@ server = Harbor::FTP::Server.new
 # on a privileged port like 21 however.
 server.port = 2121
 
+# Let's create a UserManager so we can add users to
+# our server, set their passwords, home-directories, etc.
 user_manager = Harbor::FTP::UserManagers::HashUserManager.new
 
 # Let's assume we're loading a CSV file:
 CSV::parse(DATA) do |row|
   user_manager.add_user row[0], row[1], row[2]
 end
+# NOTE: If you added an "anonymous" user, then the server
+# would allow anonymous logins. That's actually all the
+# AnonymousUserManager does. It inherits from HashUserManager,
+# and adds an "anonymous" user during initialization.
 
 # Now assign the user_manager to the server so it'll use it
 # to authorize your users.
