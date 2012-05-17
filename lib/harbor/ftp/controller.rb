87775b4e1e5fbea03fefb3c1c07a0c272c84ee31
Sam Smoot <ssmoot@gmail.com>
2012-05-17 15:36:57 -0500

    Added nodoc comments to Controller::register_command.

--- lib/harbor/ftp/controller.rb
+++ lib/harbor/ftp/controller.rb
@@ -6,6 +6,19 @@ class Harbor
         Harbor::FTP::services.set(target.name, target)
       end
       
+      # :nodoc:
+      # This is an internal method, defined here simply to reduce
+      # boiler-plate in writing new Command classes. When
+      # Harbor::FTP::Command is inherited, a hook fires passing the
+      # new descendant class to this method, which in turn adds a
+      # class-method to the Controller class enabling you to define
+      # "actions" for that Command. ie:
+      #
+      # class Example < Harbor::FTP::Controller
+      #   list "uploads" do
+      #     raise NotImplementedError.new
+      #   end
+      # end
       def self.register_command(command)
         name = File::basename(Harbor::FTP::underscore(command.name))
         (class << self; self; end).send(:define_method, name) do |*args|
